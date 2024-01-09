7228c0f65a127651bf19cce449420ba6c9747f64
Sebastian <sebastianwuczkowski@gmail.com>
2024-01-09 12:10:54 +0100

    add dropdown component

---
+++ components/app-header.vue
@@ -0,0 +1,14 @@
+
+import type { UAvatar } from '#build/components';
+
+import type { NuxtLink } from '#build/components';
+<template>
+    <header class="flex justify-between items-center mt-5">
+        <NuxtLink to="/" class="text-xl font-bold">
+            Finance Tracker
+        </NuxtLink>
+        <div>
+            <UAvatar alt="Logo"></UAvatar>
+        </div>
+    </header>
+</template>
