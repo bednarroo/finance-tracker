9a8882adb85524b6f825b902fdb3c8b3efd294d7
Sebastian <sebastianwuczkowski@gmail.com>
2024-01-10 20:26:15 +0100

    connect to DB

--- components/app-header.vue
+++ components/app-header.vue
@@ -1,14 +1,10 @@
-
-import type { UAvatar } from '#build/components';
-
-import type { NuxtLink } from '#build/components';
 <template>
-    <header class="flex justify-between items-center mt-5">
-        <NuxtLink to="/" class="text-xl font-bold">
-            Finance Tracker
-        </NuxtLink>
-        <div>
-            <UAvatar alt="Logo"></UAvatar>
-        </div>
+    <header class="flex justify-between items-center mt-10">
+      <NuxtLink to="/" class="text-xl font-bold">
+        Finance Tracker
+      </NuxtLink>
+      <div>
+        <UAvatar src="https://avatars.githubusercontent.com/u/739984?v=4" alt="Avatar" />
+      </div>
     </header>
-</template>
+  </template>
