ce472f6e5daae24803af19dc3d1d22d0398c41f9
Sebastian <sebastianwuczkowski@gmail.com>
2024-01-18 17:33:52 +0100

    composables

--- components/transaction.vue
+++ components/transaction.vue
@@ -1,86 +1,93 @@
 <template>
-    <div class="grid grid-cols-3 py-4 border-b border-gray-200 dark:border-gray-800 text-gray-900 dark:text-gray-100">
-      <div class="flex items-center justify-between space-x-4 col-span-2">
-        <div class="flex items-center space-x-1">
-          <UIcon :name="icon" :class="[iconColor]" />
-          <div>{{transaction.description}}</div>
-        </div>
-  
-        <div>
-          <UBadge color="white" v-if="transaction.category">{{transaction.category}}</UBadge>
-        </div>
+  <div
+    class="grid grid-cols-3 py-4 border-b border-gray-200 dark:border-gray-800 text-gray-900 dark:text-gray-100"
+  >
+    <div class="flex items-center justify-between space-x-4 col-span-2">
+      <div class="flex items-center space-x-1">
+        <UIcon :name="icon" :class="[iconColor]" />
+        <div>{{ transaction.description }}</div>
       </div>
-  
-      <div class="flex items-center justify-end space-x-2">
-        <div>{{ currency }}</div>
-        <div>
-            <UDropdown :items="items" :popper="{ placement: 'bottom-start' }">
-                <UButton color="white" label="Options" trailing-icon="i-heroicons-chevron-down-20-solid" :loading="isLoading" />
-            </UDropdown>
-        </div>
+
+      <div>
+        <UBadge color="white" v-if="transaction.category">{{
+          transaction.category
+        }}</UBadge>
       </div>
     </div>
-  </template>
+
+    <div class="flex items-center justify-end space-x-2">
+      <div>{{ currency }}</div>
+      <div>
+        <UDropdown :items="items" :popper="{ placement: 'bottom-start' }">
+          <UButton
+            color="white"
+            label="Options"
+            trailing-icon="i-heroicons-chevron-down-20-solid"
+            :loading="isLoading"
+          />
+        </UDropdown>
+      </div>
+    </div>
+  </div>
+</template>
   
   <script setup>
-  const props = defineProps({
-    transaction: Object
-  })
+const props = defineProps({
+  transaction: Object,
+});
 
-  const emit = defineEmits(['deleted'])
-  const isIncome = computed (() => props.transaction.type === "Income")
+const emit = defineEmits(["deleted"]);
+const isIncome = computed(() => props.transaction.type === "Income");
 
-  const icon = computed (
-    () => isIncome.value ? "i-heroicons-arrow-up-right" : "i-heroicons-arrow-down-left"
-  )
+const icon = computed(() =>
+  isIncome.value ? "i-heroicons-arrow-up-right" : "i-heroicons-arrow-down-left"
+);
 
-  const iconColor = computed(
-    () => isIncome.value ? "text-green-600" : "text-red-600"
-  )
-  
+const iconColor = computed(() =>
+  isIncome.value ? "text-green-600" : "text-red-600"
+);
 
-  const { currency } = useCurrency(props.transaction.amount)
+const { currency } = useCurrency(props.transaction.amount);
 
-  const isLoading = ref(false)
-  const toast = useToast()
-  const supabase = useSupabaseClient()
+const isLoading = ref(false);
+const toast = useToast();
+const supabase = useSupabaseClient();
 
-  const deleteTransaction = async () => {
-    isLoading.value = true
-    try {
-        await supabase.from('transactions')
-          .delete()
-          .eq('id', props.transaction.id)
-        toast.add({
-          title: 'Transaction deleted',
-          icon: 'i-heroicons-check-circle',
-          color: 'green'
-        })
-        emit('deleted', props.transaction.id)
-    } catch (error) {
-      toast.add({
-          title: 'Transaction deleted',
-          icon: 'i-heroicons-exclamation-circle',
-          color: 'red'
-        })
-    } finally {
-      isLoading.value = false
-    }
+const deleteTransaction = async () => {
+  isLoading.value = true;
+  try {
+    await supabase.from("transactions").delete().eq("id", props.transaction.id);
+    toast.add({
+      title: "Transaction deleted",
+      icon: "i-heroicons-check-circle",
+      color: "green",
+    });
+    emit("deleted", props.transaction.id);
+  } catch (error) {
+    toast.add({
+      title: "Transaction deleted",
+      icon: "i-heroicons-exclamation-circle",
+      color: "red",
+    });
+  } finally {
+    isLoading.value = false;
   }
+};
 
-  const items = [
-  [{
-      label: 'Edit',
-      icon: 'i-heroicons-pencil-square-20-solid',
-      click: () => console.log('Edit')
-    }
-    ],
+const items = [
   [
-  {
-      label: 'Delete',
-      icon: 'i-heroicons-trash-20-solid',
-      click: () => deleteTransaction()
-    }
-  ]
-]
-  </script>
+    {
+      label: "Edit",
+      icon: "i-heroicons-pencil-square-20-solid",
+      click: () => console.log("Edit"),
+    },
+  ],
+  [
+    {
+      label: "Delete",
+      icon: "i-heroicons-trash-20-solid",
+      click: () => deleteTransaction(),
+    },
+  ],
+];
+</script>
