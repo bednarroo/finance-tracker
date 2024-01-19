bfa087a454fe12e09bce000656aa0633ff502483
Sebastian <sebastianwuczkowski@gmail.com>
2024-01-19 18:04:05 +0100

    sing up

--- pages/login.vue
+++ pages/login.vue
@@ -1,23 +1,40 @@
 <template>
   <UCard v-if="!success">
     <template #header> Sign-in to Finance Tracker </template>
+
     <form>
       <UFormGroup
-        labe="Email"
+        label="Email"
         name="email"
         class="mb-4"
-        required="true"
+        :required="true"
         help="You will receive an email with the confirmation link"
       >
         <UInput type="email" placeholder="Email" required />
-
-        <UButton type="submit" variant="solid" color="black">Sign-in</UButton>
-        />
       </UFormGroup>
+
+      <UButton
+        type="submit"
+        variant="solid"
+        color="black"
+        @click="success = true"
+        >Sign-in</UButton
+      >
     </form>
   </UCard>
-</template>
+  <UCard v-else>
+    <template #header> Email has been sent </template>
 
-<script setup>
+    <div class="text-center">
+      <p class="mb-4">
+        We have sent an email to <strong>rmsil@email.com</strong> with a link to
+        sign-in.
+      </p>
+      <p><strong>Important:</strong> The link will expire in 5 minutes.</p>
+    </div>
+  </UCard>
+</template>
+  
+  <script setup>
 const success = ref(false);
 </script>
