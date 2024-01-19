198cb7ce946a1946b54debb3ae26b964c5da5ca6
Sebastian <sebastianwuczkowski@gmail.com>
2024-01-19 16:42:10 +0100

    fetch future transactions and and reactive props

--- components/trend.vue
+++ components/trend.vue
@@ -10,7 +10,11 @@
     <div>
       <USkeleton class="h-6 w-full" v-if="loading" />
       <div v-else class="flex space-x-1 items-center text-sm">
-        <UIcon :name="icon" class="w-6 h-6" :class="{ 'green': trendingUp, 'red': !trendingUp }" />
+        <UIcon
+          :name="icon"
+          class="w-6 h-6"
+          :class="{ green: trendingUp, red: !trendingUp }"
+        />
         <div class="text-gray-500 dark:text-gray-400">
           {{ percentageTrend }} vs last period
         </div>
@@ -25,35 +29,36 @@ const props = defineProps({
   amount: Number,
   lastAmount: Number,
   color: String,
-  loading: Boolean
-})
-const trendingUp = computed(
-  () => props.amount >= props.lastAmount
-)
-const icon = computed(
-  () => trendingUp.value ? 'i-heroicons-arrow-trending-up' : 'i-heroicons-arrow-trending-down'
-)
-const { currency } = useCurrency(props.amount)
+  loading: Boolean,
+});
+const { amount } = toRefs(props);
+const trendingUp = computed(() => props.amount >= props.lastAmount);
+const icon = computed(() =>
+  trendingUp.value
+    ? "i-heroicons-arrow-trending-up"
+    : "i-heroicons-arrow-trending-down"
+);
+const { currency } = useCurrency(amount);
 
 const percentageTrend = computed(() => {
-  if (props.amount === 0 || props.lastAmount === 0) return '∞%'
+  if (props.amount === 0 || props.lastAmount === 0) return "∞%";
 
-  const bigger = Math.max(props.amount, props.lastAmount)
-  const lower = Math.min(props.amount, props.lastAmount)
+  const bigger = Math.max(props.amount, props.lastAmount);
+  const lower = Math.min(props.amount, props.lastAmount);
 
-  const ratio = ((bigger - lower) / lower) * 100
+  const ratio = ((bigger - lower) / lower) * 100;
 
   // console.log(bigger, lower, ratio, Math.ceil(ratio))
 
-  return `${Math.ceil(ratio)}%`
-})
+  return `${Math.ceil(ratio)}%`;
+});
 </script>
 
 <style scoped>
 .green {
-  @apply text-green-600 dark:text-green-400
+  @apply text-green-600 dark:text-green-400;
 }
 .red {
-  @apply text-red-600 dark:text-red-400
+  @apply text-red-600 dark:text-red-400;
 }
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -93,11 +93,14 @@ const {
     grouped: { byDate },
   },
 } = useFetchTransactions(current);
+await refresh();
 
 const {
+  refresh: refreshPrevious,
   transactions: {
     incomeTotal: prevIncomeTotal,
     expenseTotal: prevExpenseTotal,
   },
 } = useFetchTransactions(previous);
+await refreshPrevious();
 </script>
