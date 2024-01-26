c27c923081c7d609f296b62ae5585b25d6f03d5a
Sebastian <sebastianwuczkowski@gmail.com>
2024-01-26 15:10:11 +0100

    add subpages

---
+++ pages/settings.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="grid grid-cols-6 gap-8">
+    <UVerticalNavigation :link="links" class="col-span-2">
+    </UVerticalNavigation>
+    <div class="col-span-4">
+      <NuxtPage />
+    </div>
+  </div>
+</template>
+
+<script setup>
+const links = [
+  { label: "Profile", to: "/settings/profile" },
+  { label: "Avatar", to: "/settings/avatar" },
+  { label: "Settings", to: "/settings/settings" },
+];
+</script>
