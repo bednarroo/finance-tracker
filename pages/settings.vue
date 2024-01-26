e7fb4d9b90fbd288a955c281bfb84faa7ce33aac
Sebastian <sebastianwuczkowski@gmail.com>
2024-01-26 15:11:36 +0100

    code refactor

--- pages/settings.vue
+++ pages/settings.vue
@@ -1,17 +1,25 @@
 <template>
   <div class="grid grid-cols-6 gap-8">
-    <UVerticalNavigation :link="links" class="col-span-2">
-    </UVerticalNavigation>
+    <UVerticalNavigation :links="links" class="col-span-2" />
     <div class="col-span-4">
       <NuxtPage />
     </div>
   </div>
 </template>
-
-<script setup>
+  
+  <script setup>
 const links = [
-  { label: "Profile", to: "/settings/profile" },
-  { label: "Avatar", to: "/settings/avatar" },
-  { label: "Settings", to: "/settings/settings" },
+  {
+    label: "Profile",
+    to: "/settings/profile",
+  },
+  {
+    label: "Avatar",
+    to: "/settings/avatar",
+  },
+  {
+    label: "Settings",
+    to: "/settings/settings",
+  },
 ];
 </script>
