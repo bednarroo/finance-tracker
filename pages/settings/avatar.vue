134b92eb6aae1fa6b63f4d9976e95a9a28be562b
Sebastian <sebastianwuczkowski@gmail.com>
2024-02-05 22:33:45 +0100

    add avatar

--- pages/settings/avatar.vue
+++ pages/settings/avatar.vue
@@ -53,22 +53,36 @@ const saveAvatar = async () => {
   const file = fileInput.value.input.files[0];
 
   if (!file) {
-    toastError({ title: " Select a file to upload one" });
+    toastError({ title: "Select a file to upload first" });
     return;
   }
 
   console.log(file);
 
-  const fileExt = file.name.split(".").pop;
+  const fileExt = file.name.split(".").pop();
   const fileName = `${Math.random()}.${fileExt}`;
+  console.log(fileName);
 
   try {
     uploading.value = true;
     // 1. Grab the current avatar URL
+    const currentAvatarUrl = user.value.user_metadata?.avatar_url;
     // 2. Upload the image to avatars bucket
+    const { error } = await supabase.storage
+      .from("avatars")
+      .upload(fileName, file);
+    if (error) throw error;
+
     // 3. Update the user metadata with the avatar URL
+    await supabase.auth.updateUser({
+      data: {
+        avatar_url: fileName,
+      },
+    });
     // 4. (OPTIONALLY) remove the old avatar file
+
     // 5. Reset the file input
+    fileInput.value.input.value = null;
 
     toastSuccess({
       title: "Avatar uploaded",
