0a4487b98eb2c4a00633cecb324d659e7a271640
Sebastian <sebastianwuczkowski@gmail.com>
2024-02-07 17:33:34 +0100

    fix vercel configuration

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,7 +5,7 @@ export default defineNuxtConfig({
   supabase: {
     redirect: true
   },
-  runtimeConfig:{
+  runTimeConfig:{
     public: {
       baseUrl: process.env.BASE_URL ?? 'http://localhost:3000'
     }
